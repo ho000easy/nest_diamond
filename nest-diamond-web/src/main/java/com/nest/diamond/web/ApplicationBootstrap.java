b4fb2662f57636a4d4b229b43222251c3b2f764c
ho000easy <ho000easy@gmail.com>
2025-12-02 18:44:39 +0800

    访问日志表

--- nest-diamond-web/src/main/java/com/nest/diamond/web/ApplicationBootstrap.java
+++ nest-diamond-web/src/main/java/com/nest/diamond/web/ApplicationBootstrap.java
@@ -4,9 +4,12 @@ package com.nest.diamond.web;
 import com.nest.diamond.web.checker.ApplicationStartCheck;
 import lombok.SneakyThrows;
 import org.apache.curator.test.TestingServer;
+import org.apache.dubbo.common.utils.NetUtils;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Field;
 
 @SpringBootApplication(scanBasePackages = "com.nest.diamond")
 @EnableDubbo(scanBasePackages = "com.nest.diamond")
@@ -14,6 +17,7 @@ public class ApplicationBootstrap {
 
     @SneakyThrows
     public static void main(String[] args) {
+        initialize();
         TestingServer server = new TestingServer(2181);
         server.start();
 
@@ -22,5 +26,18 @@ public class ApplicationBootstrap {
         SpringApplication.run(ApplicationBootstrap.class, args);
     }
 
+
+    private static void initialize() {
+        // 修改dubbo注册的IP为本机
+
+        try {
+            Field f = NetUtils.class.getDeclaredField("HOST_ADDRESS");
+            f.setAccessible(true);
+            f.set(null, "127.0.0.1");
+            System.out.println("⚠️  [Dubbo Host Override] 强制 Host 设置为 127.0.0.1");
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to override Dubbo HOST_ADDRESS", e);
+        }
+    }
 }
 
